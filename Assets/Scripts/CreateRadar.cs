315a94269c0ba888f2de913dfabbc4d49a8f06e6
Antonio Omazić <73439988+aomazic@users.noreply.github.com>
2024-10-25 18:35:23 +0200

    Added moving radar line

--- Assets/Scripts/CreateRadar.cs
+++ Assets/Scripts/CreateRadar.cs
@@ -8,7 +8,17 @@ public class CreateRadar : MonoBehaviour
     [Header("Number of horizontal lines")] [SerializeField]
     private int numOfHorizontal = 10;
 
+    [Header("Radar lines properties")] [SerializeField]
+    private float radarLineWidth = 0.015f;
+
+    [SerializeField] private float radarLineSpeed = 1.0f;
+
+
     [SerializeField] private GameObject linePrefab;
+    private GameObject radarLine;
+    private float viewportHeight;
+
+    private float viewportWidth;
 
     private void Start()
     {
@@ -19,9 +29,27 @@ public class CreateRadar : MonoBehaviour
             return;
         }
 
-        var viewportWidth = Camera.main.orthographicSize * 2 * Camera.main.aspect;
-        var viewportHeight = Camera.main.orthographicSize * 2;
+        viewportWidth = Camera.main.orthographicSize * 2 * Camera.main.aspect;
+        viewportHeight = Camera.main.orthographicSize * 2;
+        CreateGrid();
 
+        radarLine = Instantiate(linePrefab, new Vector3(-viewportHeight / 2, 0, 0), Quaternion.identity);
+        radarLine.transform.localScale = new Vector3(radarLineWidth * 4, viewportHeight, 1);
+    }
+
+    private void Update()
+    {
+        radarLine.transform.position += radarLineSpeed * Time.deltaTime * Vector3.right;
+
+        // Reset position if it goes out of bounds
+        if (radarLine.transform.position.x > viewportWidth / 2)
+        {
+            radarLine.transform.position = new Vector3(-viewportWidth / 2, 0, 0);
+        }
+    }
+
+    private void CreateGrid()
+    {
         var verticalSpacing = viewportWidth / (numOfVertical + 1);
         var horizontalSpacing = viewportHeight / (numOfHorizontal + 1);
 
@@ -29,14 +57,14 @@ public class CreateRadar : MonoBehaviour
         {
             var position = new Vector3(-viewportWidth / 2 + i * verticalSpacing, 0, 0);
             var line = Instantiate(linePrefab, position, Quaternion.identity);
-            line.transform.localScale = new Vector3(0.015f, viewportHeight, 1);
+            line.transform.localScale = new Vector3(radarLineWidth, viewportHeight, 1);
         }
 
         for (var i = 0; i <= numOfHorizontal + 1; i++)
         {
             var position = new Vector3(0, -viewportHeight / 2 + i * horizontalSpacing, 0);
             var line = Instantiate(linePrefab, position, Quaternion.Euler(0, 0, 90));
-            line.transform.localScale = new Vector3(0.015f, viewportWidth, 1);
+            line.transform.localScale = new Vector3(radarLineWidth, viewportWidth, 1);
         }
     }
 }
